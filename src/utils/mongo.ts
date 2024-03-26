9e68247293b4aa9b18b9b1ee782d9fd6d741f36b
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2024-03-26 15:41:18 +0200

    Remove Un-neccessary codes

--- src/utils/mongo.ts
+++ src/utils/mongo.ts
@@ -6,7 +6,6 @@ export const mongoConnect = async (uri: string) => {
     try {
         await mongoose.connect(uri);
     } catch (error) {
-        process.exit(1);
     }
 };
 
@@ -14,6 +13,5 @@ export const mongoDisconnect = async () => {
     try {
         await mongoose.disconnect();
     } catch (error) {
-        process.exit(2);
     }
 };
